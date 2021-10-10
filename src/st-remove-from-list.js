06168e6009265eea16d05cbc447bed49660a41fc
Alexander Danilov <h4rb4rd@gmail.com>
2021-10-10 23:39:14 +0300

    feat: implement st-remove-from-list

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -19,7 +19,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+module.exports = function removeKFromList(l, k) {
+  let list = l;
+
+  if (!list) {
+    return;
+  }
+
+  while (list && list.value === k) {
+    list = list.next;
+  }
+
+  let current = list;
+
+  while (current.next) {
+    if (current.next.value === k) {
+      // remove
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return list;
+};
